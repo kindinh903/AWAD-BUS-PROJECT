c60ad673f10b26d9442e1fc2962584adfabc24ed
kindinh903 <kindinh903@gmail.com>
2025-11-24 02:32:21 +0700

    devtool config

--- frontend-auth/src/main.tsx
+++ frontend-auth/src/main.tsx
@@ -4,6 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
 import './index.css';
+import { tokenManager } from './lib/tokenManager';
+
+// Declare global type for window
+declare global {
+  interface Window {
+    tokenManager: typeof tokenManager;
+  }
+}
+
+// Expose tokenManager to window for DevTools console
+window.tokenManager = tokenManager;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
